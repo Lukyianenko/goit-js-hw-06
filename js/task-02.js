b26ad7c64f7bb1626e052b08b88ab3e90e9a5d5a
Lukyianenko <110479686+Lukyianenko@users.noreply.github.com>
2022-11-27 09:28:05 +0200

    add task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingridientsEl = document.querySelector('ul');
+console.log(ingridientsEl);
+
+const elements = ingredients.map((named) => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = named;
+  itemEl.classList.add('item');
+
+  return itemEl;
+  });
+
+  ingridientsEl.append(...elements);
