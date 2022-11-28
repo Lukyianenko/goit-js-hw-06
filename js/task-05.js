e9d92d3e8618a9d468ba751525b430af0f97f92a
Lukyianenko <110479686+Lukyianenko@users.noreply.github.com>
2022-11-28 14:36:23 +0200

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,14 @@
+const inputEl = document.querySelector('#name-input');
+const nameLabelEl = document.querySelector('#name-output');
+
+console.log(nameLabelEl);
+
+inputEl.addEventListener('input', onInputChange);
+
+function onInputChange(event) {
+    
+    nameLabelEl.textContent = event.currentTarget.value;
+    if (event.currentTarget.value === "") {
+        nameLabelEl.textContent = 'Anonymous';
+    };
+}
