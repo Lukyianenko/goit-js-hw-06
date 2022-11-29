5b6fce26bec7a94c0dd7a3df03bcbc7d564f62b6
Lukyianenko <110479686+Lukyianenko@users.noreply.github.com>
2022-11-29 22:56:09 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -8,23 +8,39 @@ const buttonDestroyEl = document.querySelector('button[data-destroy]');
 const boxEl = document.querySelector('#boxes');
 
 buttonCreateEl.addEventListener('click', numAmount);
+buttonDestroyEl.addEventListener('click', onClear);
 
-const boxesArray = [];
 
-function numAmount() {
+
+function numAmount(event) {
+
+const boxesArray = []; 
   const amount = inputEl.value; 
   console.log(amount); 
+  
 
   for (let i = 0; i < amount; i += 1) {
+    
     let widthNum = 30; 
     let heightNum = 30;
     widthNum += i * 10;
     heightNum += i * 10;
-const divs = `<div style= width: ${widthNum}; height: ${heightNum}; background-color: ${getRandomHexColor()};></div>`;
+const divs = document.createElement('div');
+divs.classList.add('box1')
+ divs.style.width = widthNum +'px';
+ divs.style.height = heightNum +'px';
+ divs.style.backgroundColor = getRandomHexColor();
+ 
    boxesArray.push(divs);
  }
+ boxEl.append(...boxesArray);
 }
 
-console.log(boxesArray);
-const boxesEachElUnite = boxesArray.join("");
-boxEl.insertAdjacentHTML("afterbegin", boxesEachElUnite);
+function onClear() {
+  const boxDel = document.querySelectorAll('.box1');
+  console.log(boxDel);
+  for (let el of boxDel) {
+    el.remove();
+  }
+}
+
