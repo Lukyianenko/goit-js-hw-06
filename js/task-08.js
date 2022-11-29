2bd498b6afc1a4f0ea3965f8dde0b6555ea7dd52
Lukyianenko <110479686+Lukyianenko@users.noreply.github.com>
2022-11-29 17:48:04 +0200

    task 9 add

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
 
+const form = document.querySelector('.login-form');
+
+const users = {
+    email: "",
+    password: "",
+}
+
+form.addEventListener('submit', onFormSubmit);
+
+ function onFormSubmit(event) {
+    event.preventDefault();
+
+    const mail = event.currentTarget.elements.email.value;
+    const password = event.currentTarget.elements.password.value;
+
+    if (mail === "" || password === "") {
+        alert('Всі поля повинні бути заповнені!');
+    } else {
+        users.email = mail;
+        users.password = password;
+    }
+
+    console.log(users);
+    form.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+console.log(getRandomHexColor());
+
+const bodyEl = document.querySelector('body');
+const spanEl = document.querySelector('.color');
+const btnEl = document.querySelector('.change-color');
+
+btnEl.addEventListener('click', onChangeColorBg);
+
+function onChangeColorBg() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+}
